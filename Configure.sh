2f530a95fff4d99263f5e60d2fdc6c8a34fcfb55
Bot_Ninja <85632248+NinjaBot17@users.noreply.github.com>
2021-06-11 08:41:43 +0900

    Update Configure.sh

--- Configure.sh
+++ Configure.sh
@@ -1,9 +1,12 @@
 echo ""
 echo ""
 
+python3 -m pip install requests bs4
+python3 -m pip install mechanize
 python3 -m pip install twine
 python3 -m pip install Mechanicalsoup
-
+python3 -m pip install selenium
+python3 -m pip install clang
 
 echo ""
 echo ""
