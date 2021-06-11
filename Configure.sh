d67ff845e2e36670a6c2f15b084cc0583be121c1
Bot_Ninja <85632248+NinjaBot17@users.noreply.github.com>
2021-06-12 00:13:13 +0900

    Update Configure.sh

--- Configure.sh
+++ Configure.sh
@@ -7,6 +7,13 @@ python3 -m pip install twine
 python3 -m pip install Mechanicalsoup
 python3 -m pip install selenium
 python3 -m pip install clang
+python3 -m pip install socialbrute
+python3 -m pip install brute
+python3 -m pip install bruter
+python3 -m pip install rtspbrute
+python3 -m pip install bruteforceDict
+python3 -m pip install sshame
+python3 -m pip install stegcracker
 
 echo ""
 echo ""
